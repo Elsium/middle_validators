a1921bd73f939815a100ca933074cd70c39af1c0
Dima <leedmitry8091@gmail.com>
2024-04-19 15:41:46 +1100

    ready for the build to deploy

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,8 +4,8 @@ export default function NotFound() {
     return (
         <div className='flex justify-center items-center'>
             <div className='mx-auto text-white flex items-center font-roboto'>
-                <p className='text-2xl'>404</p>
-                <div className='h-16 w-1 border-l border-l-gray-50 mx-4'/>
+                <p className='text-xl'>404</p>
+                <div className='h-[20px] w-1 border-l border-l-gray-50 mx-4'/>
                 <p className='text-xm'>Page not found<br/>Return <Link className='text-amber-500 hover:underline' href={`/`}>
                     back
                 </Link></p>
